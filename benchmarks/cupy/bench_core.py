e166250182e33f745c8f2e72445e67e48e1b142f
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2018-06-22 17:29:39 +0900

    more parameterize

--- benchmarks/cupy/bench_core.py
+++ benchmarks/cupy/bench_core.py
@@ -8,13 +8,14 @@ import numpy
 
 @sync
 @parameterize([
-    ('ndim', [1, 2, 5, 8]),
+    ('dtype', ['float32', 'complex128']),
+    ('ndim', [0, 1, 2, 5, 8]),
     ('in_order', ['C', 'F']),
     ('out_order', ['C', 'F']),
 ])
 class Array(BenchmarkBase):
-    def setup(self, ndim, in_order, out_order):
-        self.x = numpy.zeros((10,) * ndim, order=in_order)
+    def setup(self, dtype, ndim, in_order, out_order):
+        self.x = numpy.zeros((10,) * ndim, dtype=dtype, order=in_order)
 
-    def time_array_from_numpy(self, ndim, in_order, out_order):
+    def time_array_from_numpy(self, dtype, ndim, in_order, out_order):
         cupy.array(self.x, order=out_order)
